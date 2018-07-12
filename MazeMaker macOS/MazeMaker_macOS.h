7c17e5b2afac1ea12c9f971b5a3afa3239ccfc61
tothgy <tothgy@users.noreply.github.com>
2018-07-12 14:28:42 +0200

    Add macOS target

---
+++ MazeMaker macOS/MazeMaker_macOS.h
@@ -0,0 +1,19 @@
+//
+//  MazeMaker_macOS.h
+//  MazeMaker macOS
+//
+//  Created by Gyorgy Toth on 12/07/2018.
+//  Copyright © 2018 Jamis Buck. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MazeMaker_macOS.
+FOUNDATION_EXPORT double MazeMaker_macOSVersionNumber;
+
+//! Project version string for MazeMaker_macOS.
+FOUNDATION_EXPORT const unsigned char MazeMaker_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MazeMaker_macOS/PublicHeader.h>
+
+
