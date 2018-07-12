cb7a844ec2bd6c4861a7605662d1e8a65f48677e
tothgy <tothgy@users.noreply.github.com>
2018-07-12 17:43:21 +0200

    Duplicate Geometry Generator classes for AppKit use

---
+++ MazeMaker macOS/OrthoLinewiseGeometryGenerator.swift
@@ -0,0 +1,50 @@
+open class OrthoLinewiseGeometryGenerator : GeometryGenerator {
+  let grid: Grid
+  let layout: OrthogonalLayout
+  let scale: CGFloat
+  let margin: CGFloat
+
+  open let bounds: CGRect
+
+  public required init(grid: Grid, scale: CGFloat, margin: CGFloat) {
+    self.grid = grid
+    self.scale = scale
+    self.layout = grid.layout as! OrthogonalLayout
+    self.margin = margin
+    self.bounds = CGRect(x: 0, y: 0,
+      width: CGFloat(layout.columns - 1) * scale + margin*2,
+      height: CGFloat(layout.rows - 1) * scale + margin*2)
+  }
+
+  open func render(_ ctx: CGContext) {
+    NSColor.black.setStroke()
+    NSColor.white.setFill()
+
+    ctx.fill(bounds)
+
+    ctx.setLineCap(CGLineCap.round)
+    ctx.setLineWidth(1.0)
+
+    for cell in grid.cells {
+      if let cell = cell as? OrthogonalCell {
+        let x = CGFloat(cell.gridLocation.column) * scale + margin
+        let y = CGFloat(cell.gridLocation.row) * scale + margin
+
+        let x2 = CGFloat(cell.gridLocation.column+1) * scale + margin
+        let y2 = CGFloat(cell.gridLocation.row+1) * scale + margin
+
+        if cell.isLinkedWith(cell.south) {
+          ctx.move(to: CGPoint(x: x, y: y))
+          ctx.addLine(to: CGPoint(x: x, y: y2))
+        }
+
+        if cell.isLinkedWith(cell.east) {
+          ctx.move(to: CGPoint(x: x, y: y))
+          ctx.addLine(to: CGPoint(x: x2, y: y))
+        }
+      }
+    }
+    
+    ctx.strokePath()
+  }
+}
---
+++ MazeMaker macOS/PolarWallwiseGeometryGenerator.swift
@@ -0,0 +1,63 @@
+open class PolarWallwiseGeometryGenerator : GeometryGenerator {
+  let grid: Grid
+  let layout: PolarLayout
+  let scale: CGFloat
+  let margin: CGFloat
+
+  open let bounds: CGRect
+
+  public required init(grid: Grid, scale: CGFloat, margin: CGFloat) {
+    self.grid = grid
+    self.scale = scale
+    self.layout = grid.layout as! PolarLayout
+    self.margin = margin
+    self.bounds = CGRect(x: 0, y: 0,
+      width: CGFloat(layout.rings * 2) * scale + margin*2,
+      height: CGFloat(layout.rings * 2) * scale + margin*2)
+  }
+
+  open func render(_ ctx: CGContext) {
+    NSColor.black.setStroke()
+    NSColor.white.setFill()
+
+    ctx.fill(bounds)
+
+    ctx.setLineCap(CGLineCap.round)
+    ctx.setLineWidth(1.0)
+
+    let ox = bounds.size.width / 2
+    let oy = bounds.size.height / 2
+
+    for cell in grid.cells {
+      if let cell = cell as? PolarCell {
+        let theta = CGFloat(2 * M_PI) / CGFloat(layout.ringSizeAt(cell.polarLocation.ring))
+        let innerRadius = CGFloat(cell.polarLocation.ring) * scale
+        let outerRadius = CGFloat(cell.polarLocation.ring+1) * scale
+        let thetaCCW = CGFloat(cell.polarLocation.spoke) * theta
+        let thetaCW = CGFloat(cell.polarLocation.spoke+1) * theta
+
+        let ax = ox + innerRadius * cos(thetaCCW)
+        let ay = oy + innerRadius * sin(thetaCCW)
+        let bx = ox + outerRadius * cos(thetaCCW)
+        let by = oy + outerRadius * sin(thetaCCW)
+
+        if cell.outward.isEmpty {
+          ctx.move(to: CGPoint(x: bx, y: by))
+            ctx.addArc(center: CGPoint(x: ox, y: oy), radius: outerRadius, startAngle: thetaCCW, endAngle: thetaCW, clockwise: false)
+        }
+
+        if !cell.isLinkedWith(cell.inward) {
+          ctx.move(to: CGPoint(x: ax, y: ay))
+            ctx.addArc(center: CGPoint(x: ox, y: oy), radius: innerRadius, startAngle: thetaCCW, endAngle: thetaCW, clockwise: false)
+        }
+
+        if !cell.isLinkedWith(cell.ccw) && cell != cell.ccw {
+          //CGContextMoveToPoint(ctx, ax, ay)
+          //CGContextAddLineToPoint(ctx, bx, by)
+        }
+      }
+    }
+    
+    ctx.strokePath()
+  }
+}
